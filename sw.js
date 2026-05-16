7b519e7e00e14f0232f23ea0a64ef076d5a76261
PedroOllersSoler <bombacampos81@gmail.com>
2026-05-16 21:16:22 +0200

    chore: SW ojpc-v118 -> ojpc-v119 para forzar recarga app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ojpc-v118';
-const ASSETS=['./',  './manifest.json'];
+const CACHE='ojpc-v119';
+const ASSETS=['./', './manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.url.includes('index.html')||e.request.url.endsWith('/')){e.respondWith(fetch(e.request).then(r=>{const rc=r.clone();caches.open(CACHE).then(c=>c.put(e.request,rc));return r;}).catch(()=>caches.match(e.request)));} else {e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
+self.addEventListener('fetch',e=>{if(e.request.url.includes('index.html')||e.request.url.endsWith('/')){e.respondWith(fetch(e.request).then(r=>{const rc=r.clone();caches.open(CACHE).then(c=>c.put(e.request,rc));return r;}));}});
